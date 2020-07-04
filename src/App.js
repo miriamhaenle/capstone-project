1c412440343f87c299cfcb541e08b45aece09694
Miriam <miriam.haenle@gmail.com>
2020-07-04 16:53:24 +0200

    Save total carbon footprint to local storage and get on app reload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import SumCarbonFootPrint from './components/SumCarbonFootprint/SumCarbonFootprint'
 import AddKilometers from './components/AddKilometers/AddKilometers'
 
 function App() {
   const [totalFootprint, setTotalFootprint] = useState(0)
+
+  useEffect(() => {
+    const lastSavedCarbonFootprint = JSON.parse(
+      localStorage.getItem('Carbon Footprint')
+    )
+    setTotalFootprint(lastSavedCarbonFootprint)
+  }, [])
+  useEffect(() => {
+    localStorage.setItem('Carbon Footprint', JSON.stringify(totalFootprint))
+  }, [totalFootprint])
+
   return (
     <div className="App">
       <SumCarbonFootPrint totalFootprint={totalFootprint}></SumCarbonFootPrint>
