adecb1aa45a1332a01040411a9963f9143ce856e
Miriam <miriam.haenle@gmail.com>
2020-07-05 21:05:54 +0200

    Add describe

--- src/components/Button/buttonDisabled.test.js
+++ src/components/Button/buttonDisabled.test.js
@@ -1,9 +1,11 @@
 import Button from './Button'
 
-test('Return true when number smaller 1', () => {
-  expect(0.2 >= 1 ? false : true).toBeTruthy()
-})
+describe('check if button should be disabled', () => {
+  test('Return true when number smaller 1', () => {
+    expect(0.2 >= 1 ? false : true).toBeTruthy()
+  })
 
-test('Return false when number bigger 1', () => {
-  expect(2 >= 1 ? false : true).toBeFalsy()
+  test('Return false when number bigger 1', () => {
+    expect(2 >= 1 ? false : true).toBeFalsy()
+  })
 })
