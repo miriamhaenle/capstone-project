f6e6a989c35b01809ccf88ae2a31e4f6305cadc8
Miriam <miriam.haenle@gmail.com>
2020-07-05 20:02:00 +0200

    Add several unit tests to calculateTotalFootprintSum

--- src/components/calculateTotalFootprintSum.test.js
+++ src/components/calculateTotalFootprintSum.test.js
@@ -1,5 +1,17 @@
 import { calculateTotalFootprintSum } from '../App'
 
-test('adds up items of array', () => {
+test('adding up numbers of array', () => {
   expect(calculateTotalFootprintSum([3, 2, 1])).toBe(6)
+})
+
+test('adding up numbers of array with negative numbers', () => {
+  expect(calculateTotalFootprintSum([-5, 3, 1])).toBe(-1)
+})
+
+test('if value is null undefined is returned', () => {
+  expect(calculateTotalFootprintSum(null)).toBeUndefined()
+})
+
+test('returning empty array', () => {
+  expect(calculateTotalFootprintSum([])).toBe(0)
 })
