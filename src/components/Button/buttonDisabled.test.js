a62618914adb9d452262ee5949a1f3b8e17fc9f0
Miriam <miriam.haenle@gmail.com>
2020-07-06 11:39:11 +0200

    Add component test for button

--- src/components/Button/buttonDisabled.test.js
+++ src/components/Button/buttonDisabled.test.js
@@ -1,4 +1,32 @@
 import Button from './Button'
+import React from 'react'
+import { render, unmountComponentAtNode } from 'react-dom'
+import { act } from 'react-dom/test-utils'
+
+let container = null
+beforeEach(() => {
+  container = document.createElement('div')
+  document.body.appendChild(container)
+})
+
+afterEach(() => {
+  unmountComponentAtNode(container)
+  container.remove()
+  container = null
+})
+
+describe('button behaviour based on input value', () => {
+  it('renders with or without text', () => {
+    act(() => {
+      render(<Button />, container)
+    })
+    expect(container.textContent).toBe('')
+    act(() => {
+      render(<Button text="Add trip" />, container)
+    })
+    expect(container.textContent).toBe('Add trip')
+  })
+})
 
 describe('check if button should be disabled', () => {
   test('Return true when number smaller 1', () => {
