1f60505924f1b57e8c730d68a30b7e6a73d62f8a
Miriam <miriam.haenle@gmail.com>
2020-07-06 11:08:13 +0200

    Add cypress test for reloading the app

--- cypress/integration/input/addKilometers.spec.js
+++ cypress/integration/input/addKilometers.spec.js
@@ -14,6 +14,16 @@ describe('user adds 0 as number for kilometers', () => {
     cy.get('[data-cy="addTrip"]').should('be.disabled')
   })
 })
+describe('on app reload the footprint stays', () => {
+  it('should still show', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('[name="distance"]').type('30')
+    cy.get('[data-cy="addTrip"]').click()
+    cy.get('[data-cy="sumFootprint"]').contains('52')
+    cy.reload()
+    cy.get('[data-cy="sumFootprint"]').contains('52')
+  })
+})
 
 describe('user adds negative number as kilometers', () => {
   it('should not do anything and button should stay disabled', () => {
@@ -28,7 +38,6 @@ describe('user adds more than 7 digits', () => {
     cy.visit('http://localhost:3000/')
     cy.get('[name="distance"]').type('1234567892137714')
     cy.get('[data-cy="addTrip"]').click()
-
     cy.get('[data-cy="sumFootprint"]').contains('21596563')
   })
 })
