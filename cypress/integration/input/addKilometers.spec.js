aad16445d87ee24551b35748f102c4bc7dcc5842
Miriam <miriam.haenle@gmail.com>
2020-07-06 10:41:25 +0200

    Add cypress tests for adding km and updating carbon footprint

--- cypress/integration/input/addKilometers.spec.js
+++ cypress/integration/input/addKilometers.spec.js
@@ -1,8 +1,34 @@
-describe('user adds kilometers of last trip', () => {
+describe('user successfully adds kilometers of last trip', () => {
   it('should update the carbon footprint after submit', () => {
     cy.visit('http://localhost:3000/')
     cy.get('[name="distance"]').type('3')
     cy.get('[data-cy="addTrip"]').click()
     cy.get('[data-cy="sumFootprint"]').contains('5')
+  })
+})
+
+describe('user adds 0 as number for kilometers', () => {
+  it('should not do anything and button should stay disabled', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('[name="distance"]').type('0')
+    cy.get('[data-cy="addTrip"]').should('be.disabled')
+  })
+})
+
+describe('user adds negative number as kilometers', () => {
+  it('should not do anything and button should stay disabled', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('[name="distance"]').type('-3')
+    cy.get('[data-cy="addTrip"]').should('be.disabled')
+  })
+})
+
+describe('user adds more than 7 digits', () => {
+  it('should only allow 7 digits and calculate with these', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('[name="distance"]').type('1234567892137714')
+    cy.get('[data-cy="addTrip"]').click()
+
+    cy.get('[data-cy="sumFootprint"]').contains('21596563')
   })
 })
