3e5f1aeb958789d1f8e9b485471a9f5743388886
Miriam <miriam.haenle@gmail.com>
2020-07-07 10:55:50 +0200

    Update devDependencies

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import GlobalStyles from '../src/components/GlobalStyles'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,3 +11,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+serviceWorker.unregister()
