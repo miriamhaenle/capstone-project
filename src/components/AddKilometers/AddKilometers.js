71106fd4c3615a2eec72bdd2c3d0ab77f6d9f140
Miriam <miriam.haenle@gmail.com>
2020-07-07 12:23:14 +0200

    Add basic styling for transportation buttons

--- src/components/AddKilometers/AddKilometers.js
+++ src/components/AddKilometers/AddKilometers.js
@@ -2,6 +2,10 @@ import React from 'react'
 import styled from 'styled-components'
 import Button from '../Button/Button'
 import useForm from '../utils/useForm'
+import car from '../../images/car.svg'
+import bus from '../../images/bus.svg'
+import train from '../../images/train.svg'
+import plane from '../../images/plane.svg'
 
 export default function AddKilometers({ updateCarbonFootprint }) {
   const [values, handleChange] = useForm({})
@@ -9,10 +13,30 @@ export default function AddKilometers({ updateCarbonFootprint }) {
   return (
     <StyledAddKilometers onSubmit={submitHandler}>
       <h2>Add new trip</h2>
+      <p>Select mode of transportation</p>
+      <TransportationSelection>
+        <TransportationType>
+          <img src={car} alt="car" />
+          Car
+        </TransportationType>
+        <TransportationType>
+          <img src={bus} alt="bus" />
+          Bus
+        </TransportationType>
+        <TransportationType>
+          <img src={train} alt="train" />
+          Train
+        </TransportationType>
+        <TransportationType>
+          <img src={plane} alt="plane" />
+          Plane
+        </TransportationType>
+      </TransportationSelection>
       <p>
         How many kilometers did it take you to get to your last race / training
         camp?
       </p>
+
       <label>
         Kilometers
         <input
@@ -75,5 +99,28 @@ const StyledAddKilometers = styled.form`
     color: var(--dusk);
     margin: 30px 0;
     padding: 5px;
+  }
+`
+const TransportationSelection = styled.div`
+  display: flex;
+  justify-content: space-around;
+  padding: 38px 0;
+`
+
+const TransportationType = styled.span`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 61px;
+  height: 61px;
+  border-radius: 50%;
+  background-color: var(--sand);
+  font-size: 14px;
+  color: var(--woodland);
+
+  img {
+    width: 26px;
+    height: 26px;
   }
 `
