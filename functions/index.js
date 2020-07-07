e51fb5988524db3de1ab0e9c5140ca9ccdc46d15
Miriam <miriam.haenle@gmail.com>
2020-07-07 09:13:07 +0200

    Add get request to get carbon footprint from api

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,39 @@
 const functions = require('firebase-functions')
 const { region } = require('firebase-functions')
+const admin = require('firebase-admin')
+const express = require('express')
+const cors = require('cors')
+const axios = require('axios')
+const serviceAccount = require('./serviceAccountKey.json')
+const app = express()
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  region('europe-west3')
-  response.send('Hello from Firebase!')
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://capstone-project-c74dc.firebaseio.com',
 })
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (reqest, response) => {
+  return response.status(200).send('Hello World')
+})
+app.get('/my-carbon-footprint', (request, response) => {
+  const queryParams = request.query
+
+  const footprint = axios
+    .get('https://api.triptocarbon.xyz/v1/footprint', {
+      params: {
+        activity: queryParams.activity,
+        activityType: queryParams.activityType,
+        fuelType: queryParams.fuelType,
+        country: queryParams.country,
+        mode: queryParams.mode,
+      },
+    })
+    .then((response) => response.data)
+    .then((footprint) => response.status(200).send(console.log(footprint)))
+    .catch((error) => console.log(error))
+})
+
+exports.app = functions.region('europe-west3').https.onRequest(app)
