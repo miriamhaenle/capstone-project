bc03a0b2b7ca3b1191aa242bbd2fef5f99e0549c
Miriam <miriam.haenle@gmail.com>
2020-07-10 21:08:56 +0200

    Update cypress test

--- cypress/integration/input/addKilometers.spec.js
+++ cypress/integration/input/addKilometers.spec.js
@@ -1,43 +1,41 @@
 describe('user successfully adds kilometers of last trip', () => {
   it('should update the carbon footprint after submit', () => {
     cy.visit('http://localhost:3000/')
+    cy.get('#car').click({ force: true })
     cy.get('[name="distance"]').type('3')
     cy.get('[data-cy="addTrip"]').click()
-    cy.get('[data-cy="sumFootprint"]').contains('5')
+    cy.get('[data-cy="sumFootprint"]').contains('0.77')
+  })
+
+  it('should still show the data as before', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('#car').click({ force: true })
+    cy.get('[name="distance"]').type('30')
+    cy.get('[data-cy="addTrip"]').click()
+    cy.get('[data-cy="sumFootprint"]').contains('7.72')
+    cy.reload()
+    cy.get('[data-cy="sumFootprint"]').contains('7.72')
+  })
+  it('should only allow 7 digits and calculate with these', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('#car').click({ force: true })
+    cy.get('[name="distance"]').type('1234567892137714')
+    cy.get('[data-cy="addTrip"]').click()
+    cy.get('[data-cy="sumFootprint"]').contains('3176542.95')
   })
 })
 
-describe('user adds 0 as number for kilometers', () => {
+describe('No updates should happen', () => {
   it('should not do anything and button should stay disabled', () => {
     cy.visit('http://localhost:3000/')
+    cy.get('#car').click({ force: true })
     cy.get('[name="distance"]').type('0')
     cy.get('[data-cy="addTrip"]').should('be.disabled')
   })
-})
-describe('on app reload the footprint stays', () => {
-  it('should still show', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('[name="distance"]').type('30')
-    cy.get('[data-cy="addTrip"]').click()
-    cy.get('[data-cy="sumFootprint"]').contains('52')
-    cy.reload()
-    cy.get('[data-cy="sumFootprint"]').contains('52')
-  })
-})
-
-describe('user adds negative number as kilometers', () => {
   it('should not do anything and button should stay disabled', () => {
     cy.visit('http://localhost:3000/')
+    cy.get('#car').click({ force: true })
     cy.get('[name="distance"]').type('-3')
     cy.get('[data-cy="addTrip"]').should('be.disabled')
-  })
-})
-
-describe('user adds more than 7 digits', () => {
-  it('should only allow 7 digits and calculate with these', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('[name="distance"]').type('1234567892137714')
-    cy.get('[data-cy="addTrip"]').click()
-    cy.get('[data-cy="sumFootprint"]').contains('21596563')
   })
 })
--- src/components/utils/calculateCarbonEmission.js
+++ src/components/utils/calculateCarbonEmission.js
@@ -1,19 +1,18 @@
 import axios from 'axios'
+const apiURL =
+  'http://localhost:5001/capstone-project-c74dc/europe-west3/app/my-carbon-footprint'
 
 export async function calculateCarbonEmission(distance, transportationType) {
   const distanceInMiles = distance * 0.62
   try {
-    const response = await axios.get(
-      'http://localhost:5001/capstone-project-c74dc/europe-west3/app/my-carbon-footprint',
-      {
-        params: {
-          activity: Number(distanceInMiles),
-          activityType: 'miles',
-          country: 'def',
-          mode: transportationType,
-        },
-      }
-    )
+    const response = await axios.get(apiURL, {
+      params: {
+        activity: Number(distanceInMiles),
+        activityType: 'miles',
+        country: 'def',
+        mode: transportationType,
+      },
+    })
 
     if (!response.data) {
       return Math.round(((distanceInMiles * 52) / 19.4) * (100 / 95))
