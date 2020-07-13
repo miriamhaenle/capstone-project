57eaed457bfc7fc053ecffd4f8f4e3654b098838
Miriam <miriam.haenle@gmail.com>
2020-07-13 21:44:01 +0200

    Basic implementation of carbon saved message

--- src/pages/SportsActivitiesPage.js
+++ src/pages/SportsActivitiesPage.js
@@ -1,25 +1,32 @@
 import React, { useState } from 'react'
 import AddKilometersForm from '../components/AddKilometers/AddKilometersForm'
+import { calculateCarbonEmission } from '../components/utils/calculateCarbonEmission'
 
 export default function SportsActivitiesPage() {
   const [sportsType, setSportsType] = useState('')
+  const [footPrintSaved, setfootPrintSaved] = useState(0)
 
   return (
-    <AddKilometersForm
-      headline="Add new activity"
-      paragraph="How many kilometers did you move outside today?"
-      updateData={updateSportsType}
-      type="sportsType"
-      getKilometers={calculateCarbonSaved}
-    ></AddKilometersForm>
+    <>
+      <p>You're awesome! You just saved {footPrintSaved} kg CO2</p>
+
+      <AddKilometersForm
+        headline="Add new activity"
+        paragraph="How many kilometers did you move outside today?"
+        updateData={updateSportsType}
+        type="sportsType"
+        getKilometers={calculateCarbonSaved}
+      ></AddKilometersForm>
+    </>
   )
 
   function updateSportsType(value) {
-    console.log(value)
     setSportsType(value)
   }
 
-  function calculateCarbonSaved({ distance }) {
-    console.log(distance, sportsType)
+  async function calculateCarbonSaved({ distance }) {
+    const carbonFootprint = await calculateCarbonEmission(distance, 'anyCar')
+    console.log(distance, sportsType, carbonFootprint)
+    setfootPrintSaved(carbonFootprint)
   }
 }
