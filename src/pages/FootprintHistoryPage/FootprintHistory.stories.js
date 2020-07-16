1a757fcec44a3c70a3c38bad23f2047467345b9b
Miriam <miriam.haenle@gmail.com>
2020-07-16 20:08:28 +0200

    Update gootprint history page story

--- src/pages/FootprintHistoryPage/FootprintHistory.stories.js
+++ src/pages/FootprintHistoryPage/FootprintHistory.stories.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import FootprintHistoryPage from './FootprintHistoryPage'
+import { withKnobs, object } from '@storybook/addon-knobs'
 
 export default {
   title: 'Footprint History Page',
   component: FootprintHistoryPage,
+  decorators: [withKnobs],
 }
-
+const footprintData = [
+  { label: 'car', y: 80 },
+  { label: 'bus', y: 20 },
+  { label: 'train', y: 100 },
+  { label: 'plane', y: 300 },
+]
 export const footprintHistoryPage = () => (
-  <FootprintHistoryPage
-    data={[
-      { label: 'car', y: 90 },
-      { label: 'bus', y: 200 },
-      { label: 'train', y: 300 },
-      { label: 'plane', y: 400 },
-    ]}
-  />
+  <FootprintHistoryPage data={object('footprint data', footprintData)} />
 )
