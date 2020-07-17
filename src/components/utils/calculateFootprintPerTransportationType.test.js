5cfd33ba8526a4e0aa7dc3ef97d59d28d7cd33d7
Miriam <miriam.haenle@gmail.com>
2020-07-17 15:19:00 +0200

    Add tets for calculate footprint per transportation type function

--- src/components/utils/calculateFootprintPerTransportationType.test.js
+++ src/components/utils/calculateFootprintPerTransportationType.test.js
@@ -1,7 +1,23 @@
-import calculateFootprintPerTransportionType from './calculateFootprintPerTransportationType'
+import { calculateFootprintPerTransportionType } from './calculateFootprintPerTransportationType'
 
 describe('Calculate footprint per transportation type', () => {
   it('should update existing state', () => {
-    expect()
+    expect(
+      calculateFootprintPerTransportionType(
+        [{ transportationType: 'car', sum: 200 }],
+        { type: 'car', sum: 200 }
+      )
+    ).toEqual([{ transportationType: 'car', sum: 400 }])
+  })
+  it('should update add a new state', () => {
+    expect(
+      calculateFootprintPerTransportionType(
+        [{ transportationType: 'car', sum: 200 }],
+        { type: 'bus', sum: 400 }
+      )
+    ).toEqual([
+      { transportationType: 'car', sum: 200 },
+      { transportationType: 'bus', sum: 400 },
+    ])
   })
 })
