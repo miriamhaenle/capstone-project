335cf122392af07c1cf8cd264fb49c456e21e0b8
Miriam <miriam.haenle@gmail.com>
2020-07-18 18:19:56 +0200

    Update Sum Carbon Footprint in storybook with transition

--- src/components/SumCarbonFootprint/SumCarbonFootprint.stories.js
+++ src/components/SumCarbonFootprint/SumCarbonFootprint.stories.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import SumCarbonFootprint from './SumCarbonFootprint'
+import { withKnobs, object } from '@storybook/addon-knobs'
 
 export default {
   title: 'Key Visual',
   component: SumCarbonFootprint,
+  decorators: [withKnobs],
 }
 
-export const sumCarbonFootprint = () => (
+export const sumCarbonFootprintNoTransition = () => (
   <SumCarbonFootprint
     sumCarbonFootprint="1234"
-    bubbleStatus={{ active: false, timestamp: Date.now() }}
+    bubbleStatus={object('Toogle transition', {
+      active: false,
+      timestamp: Date.now(),
+    })}
+  ></SumCarbonFootprint>
+)
+
+export const sumCarbonFootprintWithTransition = () => (
+  <SumCarbonFootprint
+    sumCarbonFootprint="1234"
+    bubbleStatus={object('Toggle transition', {
+      active: true,
+      timestamp: Date.now(),
+    })}
   ></SumCarbonFootprint>
 )
