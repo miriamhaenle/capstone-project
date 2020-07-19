919e17abccca5aefc7c3283af6af040e12254c7a
Miriam <miriam.haenle@gmail.com>
2020-07-19 14:41:40 +0200

    Add propTypes to utils functions

--- src/components/utils/calculateTotalFootprintSum.js
+++ src/components/utils/calculateTotalFootprintSum.js
@@ -1,5 +1,11 @@
+import PropTypes from 'prop-types'
+
 export function calculateTotalFootprintSum(carbonFootprint) {
   if (carbonFootprint !== null) {
     return carbonFootprint.reduce((acc, curr) => acc + curr, 0)
   }
+}
+
+calculateTotalFootprintSum.propTypes = {
+  carbonFootprint: PropTypes.array,
 }
