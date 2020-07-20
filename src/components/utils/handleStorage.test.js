01a979255c7ab7da2efabaf8bab4470c424f1b90
Miriam <miriam.haenle@gmail.com>
2020-07-20 12:08:15 +0200

    Add localstorage mock

--- src/components/utils/handleStorage.test.js
+++ src/components/utils/handleStorage.test.js
@@ -1,25 +1,12 @@
 import { saveToStorage, getFromStorage } from './handleStorage'
 import { render, fireEvent, waitForElement } from '@testing-library/react'
-import React from 'react'
 
-describe('Save to storage', () => {
+describe('get data from storage', () => {
   beforeEach(() => {
-    Object.defineProperty(window, 'localStorage', {
-      value: {
-        getItem: jest.fn(() => null),
-        setItem: jest.fn(() => null),
-      },
-      writable: true,
-    })
-    saveToStorage('foo', 'Bar Baz')
+    window.localStorage = localStorage
   })
-
-  it('should call function on render ', () => {
-    expect(window.localStorage.getItem).toHaveBeenCalledTimes(1)
+  it('should return null if no dataset is present', () => {
+    const foo = getFromStorage('foo')
+    expect(foo).toBe(null)
   })
-
-  it('should call function on state change', () => {})
 })
-
-/* describe('Get from storage', () => {})
- */
