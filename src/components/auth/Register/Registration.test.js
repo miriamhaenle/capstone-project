e4e4ea2bcafb8a649a50051dbf4efc18da9aad9a
Miriam <miriam.haenle@gmail.com>
2020-07-21 14:03:01 +0200

    Add basic test for registration

---
+++ src/components/auth/Register/Registration.test.js
@@ -0,0 +1,34 @@
+import '@testing-library/jest-dom/extend-expect'
+import React from 'react'
+import Registration from './Registration'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+describe('Registration form', () => {
+  it('renders the form', () => {
+    const renderedComponent = render(<Registration />)
+    expect(renderedComponent).toBeTruthy()
+  })
+
+  it('should load three input fields', () => {
+    render(<Registration />)
+    const inputFields = screen.findAllByRole('textbox')
+    expect(inputFields).toHaveLength(3)
+  })
+
+  it('should render the register button', () => {
+    render(<Registration />)
+    expect(screen.getByRole('button')).toBeInTheDocument()
+  })
+
+  it('should have a disabled button by default', () => {
+    render(<Registration />)
+    expect(screen.getByText('Register').closest('button')).toBeInTheDocument()
+  })
+
+  it('should be possible to write to input field', () => {
+    render(<Registration />)
+    userEvent.type(screen.getByRole('textbox', 'Jan Frodeno'))
+    expect(screen.getByRole('textbox').toHaveValue('Jan Frodeno'))
+  })
+})
