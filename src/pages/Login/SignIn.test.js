6c5ace3c716bb0d62f08ad91e15403c71edddf15
Miriam <miriam.haenle@gmail.com>
2020-07-23 14:06:09 +0200

    Add tests to login page

--- src/pages/Login/SignIn.test.js
+++ src/pages/Login/SignIn.test.js
@@ -1,18 +1,18 @@
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import React from 'react'
-import SignUpPage from './SignUpPage'
+import SignIn from './SignIn'
 
-describe('Sign up page', () => {
+describe('Sign In page', () => {
   let renderedPage
-
   beforeEach(() => {
-    renderedPage = render(<SignUpPage />)
+    renderedPage = render(<SignIn />)
   })
 
   it('should render the page', () => {
     expect(renderedPage).toBeTruthy()
   })
+
   it('should render the headline', () => {
     expect(screen.getByRole('heading')).toBeInTheDocument()
   })
