33427ef9dad76f2d4ca3d7367c6b2fe2c867999d
Miriam <miriam.haenle@gmail.com>
2020-08-03 21:23:45 +0200

    Add edge case tests for calcualte carbon emission

--- src/services/calculateCarbonEmission.test.js
+++ src/services/calculateCarbonEmission.test.js
@@ -17,7 +17,7 @@ describe('GET /my-carbon-footprint', () => {
     expect(carbonFootprint).toEqual(16.6)
   })
 
-  it('should return 0 on false request', async () => {
+  it('should return 0 on request with empty data', async () => {
     axios.get.mockResolvedValue({
       data: {
         carbonFootprint: '',
@@ -25,6 +25,97 @@ describe('GET /my-carbon-footprint', () => {
     })
     const carbonFootprint = await calculateCarbonEmission({
       distance: '',
+      transportationType: 'anyCar',
+    })
+    expect(carbonFootprint).toEqual(0)
+  })
+
+  it('should return 0 on request with missing data', async () => {
+    axios.get.mockResolvedValue({
+      data: {
+        carbonFootprint: '',
+      },
+    })
+    const carbonFootprint = await calculateCarbonEmission({
+      transportationType: 'anyCar',
+    })
+    expect(carbonFootprint).toEqual(0)
+  })
+
+  it('should return 0 on badly formatted request(additional param)', async () => {
+    axios.get.mockResolvedValue({
+      data: {
+        carbonFootprint: '',
+      },
+    })
+    const carbonFootprint = await calculateCarbonEmission({
+      distance: '',
+      transportationType: 'anyCar',
+      additionalParamThatShouldNotExist: 'bus',
+    })
+    expect(carbonFootprint).toEqual(0)
+  })
+
+  it('should return 0 on request with wrong data type(data type: object)', async () => {
+    axios.get.mockResolvedValue({
+      data: {
+        carbonFootprint: '',
+      },
+    })
+    const carbonFootprint = await calculateCarbonEmission({
+      distance: { kilometers: 20 },
+      transportationType: 'anyCar',
+    })
+    expect(carbonFootprint).toEqual(0)
+  })
+
+  it('should return 0 on request with wrong data type(data type: array)', async () => {
+    axios.get.mockResolvedValue({
+      data: {
+        carbonFootprint: '',
+      },
+    })
+    const carbonFootprint = await calculateCarbonEmission({
+      distance: [20, 120, 50],
+      transportationType: 'anyCar',
+    })
+    expect(carbonFootprint).toEqual(0)
+  })
+
+  it('should return 0 on request with wrong data type(data type: number)', async () => {
+    axios.get.mockResolvedValue({
+      data: {
+        carbonFootprint: '',
+      },
+    })
+    const carbonFootprint = await calculateCarbonEmission({
+      distance: 30,
+      transportationType: 'anyCar',
+    })
+    expect(carbonFootprint).toEqual(0)
+  })
+
+  it('should return 0 on request with wrong data type(data type: boolen)', async () => {
+    axios.get.mockResolvedValue({
+      data: {
+        carbonFootprint: '',
+      },
+    })
+    const carbonFootprint = await calculateCarbonEmission({
+      distance: true,
+      transportationType: 'anyCar',
+    })
+    expect(carbonFootprint).toEqual(0)
+  })
+
+  it('should return 0 on request with wrong data type(data type: undefined)', async () => {
+    axios.get.mockResolvedValue({
+      data: {
+        carbonFootprint: '',
+      },
+    })
+    const carbonFootprint = await calculateCarbonEmission({
+      distance: undefined,
       transportationType: 'anyCar',
     })
     expect(carbonFootprint).toEqual(0)
