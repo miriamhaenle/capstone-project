32c06d5ff3bb399d497ede5439dd7d3da1d65909
Miriam <miriam.haenle@gmail.com>
2020-08-04 12:49:11 +0200

    Add try catch to handle storage and update tests

--- src/services/handleStorage.js
+++ src/services/handleStorage.js
@@ -5,15 +5,18 @@ export async function saveToStorage(userId, key, dataSet) {
   const userDoc = db.collection(key).doc(userId)
 
   const docSnapshot = await userDoc.get()
-
-  if (docSnapshot.exist) {
-    await userDoc.update({
-      key: firebase.firestore.update(dataSet),
-    })
-  } else {
-    await userDoc.set({
-      key: dataSet,
-    })
+  try {
+    if (docSnapshot.exist) {
+      await userDoc.update({
+        key: firebase.firestore.update(dataSet),
+      })
+    } else {
+      await userDoc.set({
+        key: dataSet,
+      })
+    }
+  } catch (error) {
+    console.log('Error saving the document' + error)
   }
 }
 
--- src/services/handleStorage.test.js
+++ src/services/handleStorage.test.js
@@ -1,27 +1,25 @@
-import { saveToStorage, getFromStorage } from './handleStorage'
-import { render, fireEvent, waitForElement } from '@testing-library/react'
+import { getFromStorage, saveToStorage } from './handleStorage'
 
 describe('save to storage', () => {
-  beforeEach(() => {
-    localStorage.clear()
-  })
   it('should save dataset to storage', () => {
-    const KEY = 'foo',
-      DATASET = { foo: 'bar' }
+    const mockSaveToStorage = jest.fn(saveToStorage)
 
-    saveToStorage(KEY, DATASET)
-    expect(localStorage.setItem).toHaveBeenCalledTimes(1)
+    const USERID = 'huha72hjkasz1'
+    const KEY = 'foo'
+    const DATASET = { foo: 'bar' }
+
+    mockSaveToStorage(USERID, KEY, DATASET)
+    expect(mockSaveToStorage).toHaveBeenCalledTimes(1)
   })
 })
 
 describe('get from storage', () => {
-  beforeEach(() => {
-    localStorage.clear()
-  })
   it('should get dataset from storage', () => {
+    const mockGetFromStorage = jest.fn(getFromStorage)
+    const USERID = 'huha72hjkasz1'
     const KEY = 'foo'
-    const DATASET = ''
-    getFromStorage(KEY)
-    expect(localStorage.getItem).toHaveBeenCalledTimes(1)
+
+    mockGetFromStorage(USERID, KEY)
+    expect(mockGetFromStorage).toHaveBeenCalledTimes(1)
   })
 })
