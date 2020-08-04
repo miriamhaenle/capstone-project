f53a6538324d55961ad4e5c99df5728879402317
Miriam <miriam.haenle@gmail.com>
2020-08-04 10:56:41 +0200

    Add more tests to mapping

--- src/components/DonutChart/mapFootprintPerTransportTypeToDonutChartData.test.js
+++ src/components/DonutChart/mapFootprintPerTransportTypeToDonutChartData.test.js
@@ -9,7 +9,7 @@ describe('Mapping function api data structure to donut chart data structure', ()
       })
     ).toEqual({ label: 'car', y: 200 })
   })
-  it('should also map if transportation type is empty', () => {
+  it('should map empty strings if transportation type is empty', () => {
     expect(
       mapFootprintPerTransportTypeToDonutChartData({
         transportationType: '',
@@ -17,10 +17,24 @@ describe('Mapping function api data structure to donut chart data structure', ()
       })
     ).toEqual({ label: '', y: '' })
   })
-  it('should also map if transportation sum is empty', () => {
+  it('should map empty strings if transportation sum is empty', () => {
     expect(
       mapFootprintPerTransportTypeToDonutChartData({
         transportationType: 'car',
+        sum: '',
+      })
+    ).toEqual({ label: '', y: '' })
+  })
+  it('should map empty stringsp if no sum is send', () => {
+    expect(
+      mapFootprintPerTransportTypeToDonutChartData({
+        transportationType: '',
+      })
+    ).toEqual({ label: '', y: '' })
+  })
+  it('should map empty strings if no transportation Type is send', () => {
+    expect(
+      mapFootprintPerTransportTypeToDonutChartData({
         sum: '',
       })
     ).toEqual({ label: '', y: '' })
