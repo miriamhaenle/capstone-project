b96df09040047f0cee18dbc06cb308f7df26d559
Miriam <miriam.haenle@gmail.com>
2020-08-05 16:43:27 +0200

    Optimisations

--- src/pages/FootprintHistoryPage/FootprintHistoryPage.js
+++ src/pages/FootprintHistoryPage/FootprintHistoryPage.js
@@ -5,7 +5,6 @@ import DonutChart from '../../components/DonutChart/DonutChart'
 import { Link, useHistory } from 'react-router-dom'
 import { mapFootprintPerTransportTypeToDonutChartData } from '../../components/DonutChart/mapFootprintPerTransportTypeToDonutChartData'
 import * as ROUTES from '../../constants/routes'
-import useDeviceDetect from '../../services/useDeviceDetect'
 
 FootprintHistoryPage.propTypes = {
   footprintPerTransportationType: PropTypes.array,
@@ -15,11 +14,7 @@ export default function FootprintHistoryPage({
   footprintPerTransportationType,
 }) {
   const [donutData, setDonutData] = useState([])
-  const [backButtonClicked, setBackButtonClicked] = useState({
-    active: false,
-    timestamp: Date.now(),
-  })
-  const { isMobile } = useDeviceDetect()
+
   const history = useHistory()
 
   useEffect(() => {
@@ -30,11 +25,9 @@ export default function FootprintHistoryPage({
   }, [footprintPerTransportationType])
 
   return (
-    <StyledSection backButtonClicked={backButtonClicked.active}>
+    <StyledSection>
       <Link to={ROUTES.ADD_TRIP}>
-        <span onTouchStart={startTransition} onTouchEnd={endTransition}>
-          Go back
-        </span>
+        <span>Go back</span>
       </Link>
       <h2>Breakdown of your carbon footprint</h2>
       {donutData.length >= 1 ? (
@@ -44,44 +37,13 @@ export default function FootprintHistoryPage({
       )}
     </StyledSection>
   )
-  function startTransition() {
-    setBackButtonClicked({ active: true, timestamp: Date.now() })
-  }
-
-  function endTransition() {
-    if (shouldNavigate(setBackButtonClicked.timestamp)) {
-      history.push(ROUTES.HOME)
-    }
-    setBackButtonClicked({ active: false, timestamp: Date.now() })
-  }
-
-  function shouldNavigate(timeButtonClicked) {
-    if (isMobile) {
-      return Date.now()
-    }
-    return Date.now() - timeButtonClicked > 50
-  }
 }
 
 const StyledSection = styled.main`
   background: var(--sand);
   height: 100vh;
   padding: 30px;
-  animation: ${(props) =>
-    props.backButtonClicked ? 'shrink 500ms forwards' : 'none'};
 
-  @keyframes shrink {
-    0% {
-      background: var(--sand);
-      color: var(--sand);
-    }
-
-    100% {
-      background: var(--woodland);
-      color: var(--sand);
-      transform: scale(0);
-    }
-  }
   h2 {
     font-family: var(--headlineFont);
   }
