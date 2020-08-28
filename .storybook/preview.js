34d66bc58328ca34e127b7c8ecc4b4ffc2e1967f
Miriam <miriam.haenle@gmail.com>
2020-08-28 12:12:10 +0200

    Initial fix for storybook with dark theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,12 +2,15 @@ import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 import { addDecorator, addParameters } from '@storybook/react'
 import React from 'react'
 import GlobalStyles from '../src/components/GlobalStyles'
+import { ThemeProvider } from 'styled-components'
+import { darkTheme, lightTheme } from 'components/Themes'
 
+const theme = 'light'
 addDecorator((storyFn) => (
-  <>
+  <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
     <GlobalStyles />
     {storyFn()}
-  </>
+  </ThemeProvider>
 ))
 
 addParameters({
